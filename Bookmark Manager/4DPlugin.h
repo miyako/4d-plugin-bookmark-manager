91de739c3bf685471e861ec8da375bd5e17c83d9
miyako <keisuke.miyako@4d.com>
2012-10-06 16:33:41 +0900

    removed redundant code in header file

--- Bookmark Manager/4DPlugin.h
+++ Bookmark Manager/4DPlugin.h
@@ -7,10 +7,3 @@
  #	2012/10/06
  #
  # --------------------------------------------------------------------------------*/
-
-
-
-// --- Bookmarks
-void BOOKMARK_Create(sLONG_PTR *pResult, PackagePtr pParams);
-void BOOKMARK_Resolve(sLONG_PTR *pResult, PackagePtr pParams);
-void BOOKMARK_Export_to_file(sLONG_PTR *pResult, PackagePtr pParams);
